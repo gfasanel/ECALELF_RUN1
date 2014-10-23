e00a59103079c6b104d6188bfbbedbc626c47a1b
gfasanel <giuseppe.fasanella@cern.ch>
2014-10-23 10:02:10 +0200

    scipt for histo making added

---
+++ ZFitter/checkPlotter.sh
@@ -0,0 +1,4 @@
+root -l -b checkPlot.C
+source publisher_check.sh
+
+#Plots are in validation_GS/check_Plots
---
+++ ZFitter/publisher_check.sh
@@ -0,0 +1,9 @@
+cp -r check_Plots_DataMC/ ~/scratch1/www/validation_GS/
+cp ~/scratch1/www/validation_GS/index.php ~/scratch1/www/validation_GS/check_Plots_DataMC
+cp ~/scratch1/www/validation_GS/index.php ~/scratch1/www/validation_GS/check_Plots_DataMC/invMass_SC_regrCorrSemiParV5_ele/
+cp ~/scratch1/www/validation_GS/index.php ~/scratch1/www/validation_GS/check_Plots_DataMC/invMass_SC_regrCorrSemiParV5_ele/1D/
+cp ~/scratch1/www/validation_GS/index.php ~/scratch1/www/validation_GS/check_Plots_DataMC/invMass_SC_regrCorrSemiParV5_ele/2D/
+
+cp ~/scratch1/www/validation_GS/index.php ~/scratch1/www/validation_GS/check_Plots_DataMC/invMass_SC/
+cp ~/scratch1/www/validation_GS/index.php ~/scratch1/www/validation_GS/check_Plots_DataMC/invMass_SC/1D/
+cp ~/scratch1/www/validation_GS/index.php ~/scratch1/www/validation_GS/check_Plots_DataMC/invMass_SC/2D/
